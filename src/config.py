3bd1c02e27c0c00bf621a9ac256b9500c8540520
Darin Webb <darinlwebb@gmail.com>
2022-04-27 15:25:16 -0700

    fix linting errors

--- src/config.py
+++ src/config.py
@@ -3,32 +3,30 @@ from dotenv import load_dotenv, find_dotenv
 
 class Config:
 
-  REDSHIFT_USERNAME = ""
-  REDSHIFT_PASSWORD = ""
-  REDSHIFT_HOSTNAME = ""
+    REDSHIFT_USERNAME = ""
+    REDSHIFT_PASSWORD = ""
+    REDSHIFT_HOSTNAME = ""
 
-  def __init__(self):
+    def __init__(self):
 
     config_vars = [
-      "REDSHIFT_USERNAME",
-      "REDSHIFT_PASSWORD",
-      "REDSHIFT_HOSTNAME"
+        "REDSHIFT_USERNAME",
+        "REDSHIFT_PASSWORD",
+        "REDSHIFT_HOSTNAME"
     ]
 
     for i, config_var in enumerate(config_vars):
-      # check to see if this was passed in as an environment variable first
-      print(f'checking {config_var}')
-      value = os.getenv(config_var)
+        # check to see if this was passed in as an environment variable first
+        value = os.getenv(config_var)
 
-      # fall back to .env file if not
-      if(value is None):
-        print(find_dotenv())
-        load_dotenv(find_dotenv())
-        value = os.environ.get(config_var)
+        # fall back to .env file if not
+        if(value is None):
+            load_dotenv(find_dotenv())
+            value = os.environ.get(config_var)
 
-      # still couldn't find it, fail
-      if(value is None):
-        raise Exception(f'{config_var} not found in environment or .env')
+        # still couldn't find it, fail
+        if(value is None):
+            raise Exception(f'{config_var} not found in environment or .env')
 
-      # self.REDSHIFT_USERNAME = user
-      setattr(self, config_var, value)
+        # self.REDSHIFT_USERNAME = user
+        setattr(self, config_var, value)
