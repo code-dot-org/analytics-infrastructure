190f3f4a0f06e622c1ca444e6dcbb2a144d38ade
Darin Webb <darinlwebb@gmail.com>
2022-04-27 15:26:22 -0700

    fix indent

--- src/config.py
+++ src/config.py
@@ -9,24 +9,24 @@ class Config:
 
     def __init__(self):
 
-    config_vars = [
-        "REDSHIFT_USERNAME",
-        "REDSHIFT_PASSWORD",
-        "REDSHIFT_HOSTNAME"
-    ]
+        config_vars = [
+            "REDSHIFT_USERNAME",
+            "REDSHIFT_PASSWORD",
+            "REDSHIFT_HOSTNAME"
+        ]
 
-    for i, config_var in enumerate(config_vars):
-        # check to see if this was passed in as an environment variable first
-        value = os.getenv(config_var)
+        for i, config_var in enumerate(config_vars):
+            # check to see if this was passed in as an environment variable first
+            value = os.getenv(config_var)
 
-        # fall back to .env file if not
-        if(value is None):
-            load_dotenv(find_dotenv())
-            value = os.environ.get(config_var)
+            # fall back to .env file if not
+            if(value is None):
+                load_dotenv(find_dotenv())
+                value = os.environ.get(config_var)
 
-        # still couldn't find it, fail
-        if(value is None):
-            raise Exception(f'{config_var} not found in environment or .env')
+            # still couldn't find it, fail
+            if(value is None):
+                raise Exception(f'{config_var} not found in environment or .env')
 
-        # self.REDSHIFT_USERNAME = user
-        setattr(self, config_var, value)
+            # self.REDSHIFT_USERNAME = user
+            setattr(self, config_var, value)
