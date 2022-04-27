43533ad89726c4702a27bce14f5087f9cf1f1fa3
Amy Gebhardt <amy.gebhardt@code.org>
2022-04-27 14:01:11 -0500

    fix: added comments

--- config.py
+++ config.py
@@ -7,12 +7,15 @@ class Config:
   DOMAIN = ""
 
   def __init__(self):
+    # check to see if this was passed in as an environment variable first
     user = os.getenv("REDSHIFT_USER")
 
+    # fall back to .env file
     if(user is None):
       load_dotenv(find_dotenv())
       user = os.environ.get("REDSHIFT_USER")
 
+    # couldn't find a REDSHIFT_USER, fail
     if(user is None):
       raise Exception("REDSHIFT_USER not found")
 
