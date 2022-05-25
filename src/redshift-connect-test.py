866090050a4ff0214eb67ad6d358009fa267672a
bakerfranke <baker@code.org>
2022-05-25 15:39:40 -0500

    added redshift connection and sql execution testing script

---
+++ src/redshift-connect-test.py
@@ -0,0 +1,17 @@
+import redshift_connector
+from config import Config
+
+CONFIG = Config()
+
+conn = redshift_connector.connect(
+     host=CONFIG.REDSHIFT_HOSTNAME,
+     database='dashboard',
+     user=CONFIG.REDSHIFT_USERNAME,
+     password=CONFIG.REDSHIFT_PASSWORD
+
+
+)
+
+result = conn.cursor().execute("SELECT * FROM dashboard_production_pii.pd_survey_questions WHERE questions ILIKE '%taught%' LIMIT 100;")
+list = result.fetchall()
+print(list)
